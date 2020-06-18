f7f664c84739581d15f4e12d4fe34d4b7fb14139
Flush-bleep <64114765+Flush-bleep@users.noreply.github.com>
2020-06-18 14:24:16 +0800

    game

---
+++ 09_PA11_MikealLeow/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    public float score = 0;
+
+    public Text displayScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        transform.position = new Vector3((transform.position.x), Mathf.Clamp(transform.position.y, -3.9f, 3.9f), transform.position.z);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            //print("asda");
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag =="Point")
+        {
+            score += 10;
+            displayScore.text = "Score: " + score;
+        }
+    }
+
+}
---
+++ 09_PA11_MikealLeow/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject;
+    float PositionY;
+
+    public GameObject[] Cubes;
+
+    private float CurrentSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentSpawn = Random.Range(0, 3);
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+
+        if (CurrentSpawn == 0)
+        {
+            Instantiate(Cubes[0], transform.position, transform.rotation);
+        }
+
+        if (CurrentSpawn == 1)
+        {
+            Instantiate(Cubes[1], transform.position, transform.rotation);
+        }
+
+        if (CurrentSpawn == 2)
+        {
+            Instantiate(Cubes[2], transform.position, transform.rotation);
+        }
+
+    }
+}
